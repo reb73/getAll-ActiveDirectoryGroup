a78839784801f71dda4e0f7303712e72aea0d177
reb73 <90497779+reb73@users.noreply.github.com>
2023-08-19 20:39:38 +0330

    Get All Active Directory Group

---
+++ getAdGroup.js
@@ -0,0 +1,81 @@
+const { getSetting } = require("../configs/app.config");
+const ActiveDirectory = require("activedirectory2");
+const { StatusCodes: HttpStatus } = require("http-status-codes")
+
+async function getAdGroup(req,res,next){
+    try {
+        const domainInfo = await getSetting()
+        const domainName = domainInfo.dataValues.domainAddress
+        const domainUser = domainInfo.dataValues.domainUser
+        const domainPass = domainInfo.dataValues.domainPass
+        baseDN = domainName.split(".");
+        const ad = new ActiveDirectory({
+            url: `ldap://${domainName}`,
+            baseDN: `dc=${baseDN[0]},dc=${baseDN[1]}`,
+            username: `${domainUser}@${domainName}`,
+            password: domainPass,
+        })
+        ad.findGroups((err, groups) => {
+            if (err) {
+                console.log(err);
+                res.status(HttpStatus.BAD_GATEWAY).json({
+                    StatusCode: HttpStatus.BAD_GATEWAY,
+                    errors: {
+                        message: "ارتباط با سرور دامین دارای مشکل می باشد"
+                    }
+                });
+            }
+            const allGroupWithUsers = [];
+            let processedGroups = 0
+            groups.forEach((group) => {
+                ad.getUsersForGroup(group.cn, (err, users) => {
+                    if (err) {
+                        res.status(HttpStatus.BAD_GATEWAY).json({
+                            StatusCode: HttpStatus.BAD_GATEWAY,
+                            errors: {
+                                message: "ارتباط با سرور دامین دارای مشکل می باشد"
+                            }
+                        });
+                    }
+                    const groupData = {
+                        GroupName: group.cn,
+                        members: users.map(user => ({
+                            username: user.sAMAccountName,
+                            firstname: user.givenName??undefined,
+                            lastname: user.sn??undefined,
+                            email: user.mail??undefined,
+                            displayname: user.displayName??undefined,
+                            description: user.description??undefined
+                        }))
+                    }
+                    const groupCondition = groupData.members.length > 0
+                        && groupData.members != 'Administrator'
+                        && groupData.GroupName != 'Denied RODC Password Replication Group'
+                        && groupData.GroupName != 'Administrators'
+                        && groupData.GroupName != 'Guests'
+                        && groupData.GroupName != 'Users'
+                        && groupData.GroupName != 'Schema Admins'
+                        && groupData.GroupName != 'Enterprise Admins'
+                        && groupData.GroupName != 'Group Policy Creator Owners'
+                    if (groupCondition) {
+                        allGroupWithUsers.push(groupData)
+                    }
+                    processedGroups++
+                    if (processedGroups === groups.length) {
+                        res.status(HttpStatus.OK).json({
+                            StatusCode: HttpStatus.OK,
+                            data: {
+                                allGroupWithUsers
+                            }
+                        });
+                    }
+                });
+            })
+        });
+    } catch (error) {
+        next(error)
+    }
+}
+module.exports = {
+    getAdGroup
+}
